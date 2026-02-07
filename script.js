a36db03021421df3e81dff4bf0210d02a80c401b
neer-max <bourasigamer@gmail.com>
2026-02-07 16:00:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var myindex=0;
+carousel();
+function carousel()
+{
+    var i;
+    var x=document.getElementsByClassName( "myslides" );
+    for(i=0;i<x.length;i++)
+    {
+        x[i].style.display="none";
+    }
+    myindex++;
+    if(myindex>x.length)
+    {
+        myindex=1;
+    }
+    x[myindex-1].style.display="block";
+    setTimeout(carousel,2000);
+}
